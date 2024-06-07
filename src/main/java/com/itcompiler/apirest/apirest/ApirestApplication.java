b6c5ee3e565463f746f79bdb08f62584743f88c9
martinll03 <martinlopez.sys@outlook.com>
2024-06-07 07:33:16 -0600

    Update ApirestApplication.java
    agregue un comentario

--- src/main/java/com/itcompiler/apirest/apirest/ApirestApplication.java
+++ src/main/java/com/itcompiler/apirest/apirest/ApirestApplication.java
@@ -2,7 +2,7 @@ package com.itcompiler.apirest.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// este es un comentario 
 @SpringBootApplication
 
 public class ApirestApplication {
