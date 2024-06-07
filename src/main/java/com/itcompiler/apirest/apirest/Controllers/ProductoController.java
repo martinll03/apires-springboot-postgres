f60b2fe974233b795e80386b798bf08aa8d7f74e
martinll03 <martinlopez@Martins-MacBook-Pro-3.local>
2024-06-07 07:30:41 -0600

    modificacion leve

--- src/main/java/com/itcompiler/apirest/apirest/Controllers/ProductoController.java
+++ src/main/java/com/itcompiler/apirest/apirest/Controllers/ProductoController.java
@@ -1,12 +1,11 @@
 package com.itcompiler.apirest.apirest.Controllers;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
